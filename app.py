3a34a3ffcddeba7ef11483565235f3c902bad865
qkrrlcjs <130217139+qkrrlcjs@users.noreply.github.com>
2023-07-09 13:46:27 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -5,14 +5,18 @@ app = Flask(__name__)
 @app.route('/')
 def main():
     return render_template('main.html')
+    
 
 @app.route('/submit',methods=['POST'])
 def submit():
     if request.method == 'POST':
         year = request.form.get('year')
         genre = request.form.get('genre')
+        song_list = [{'title':'벌써','singer':'이적'},
+                     {'title':'벌써2','singer':'이적2'},
+                     ]
         if year and genre:
-            return render_template('list.html')
+            return render_template('list.html', year=year, genre=genre, song_list=song_list)
         else:
             error_message = "연도와 장르 모두 선택해주세요."
             return render_template('main.html',error_message = error_message)
